cac56353b9f1982a40b7666bfd974c3ba7f92cbf
amit.sandhel <amit.sandhel@gmail.com>
2021-05-27 23:41:38 -0400

    a second commit of assignment2

--- assignment2/netflix_ae.py
+++ assignment2/netflix_ae.py
@@ -1,73 +1,50 @@
 #assignment 2 
 
-
-import tensorflow.keras.models as km
-import tensorflow.keras.layers as kl
-import tensorflow.keras.backend as K
-
-from tensorflow.keras.datasets import mnist
-
-#from tensorflow.python.framework.ops import disable_eager_execution
-#disable_eager_execution()
-
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.random as npr
 import pandas as pd
 import tensorflow as tf
-
+import tensorflow.keras.models as km
+import tensorflow.keras.layers as kl
+import tensorflow.keras.backend as K
+from tensorflow.keras import layers, losses
+from tensorflow.keras.models import Model
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 
-from tensorflow.keras import layers, losses
-from tensorflow.keras.models import Model
-
-import numpy.random as npr
 #slide 25 of he gan class 
 #replace False should probably be included 
 
+#a variable to determine if we are going to use fit or train_on_batch
+#default is tob ie train_on_batch
+TRAINING="tob"
 
-df = pd.read_csv("test.csv", sep=',') #, dtype=None) #, skiprows=[0])
+
+
+raw_df = pd.read_csv("test.csv", sep=',') #, dtype=None)
 #df.values
-print (type(df))
+#print (type(df))
 
 #convert all nan values to 0
-df = df.fillna(0)
+raw_df = raw_df.fillna(0)
 print ('updated df ')
-print (df.head())
-print ('raw shape: ', df.shape)
-
-#split train and testing data set by 80% 20%
-(train_data, test_data) = train_test_split(df, test_size=0.2)
-print ('train data shape:', train_data.shape)
-print ('test data shape:', test_data.shape)
-#convert the train and test arrayts to float 32
-train_data = train_data.values #.astype('float32').values
-test_data = test_data.values #.astype('float32').values
-
-#look at the array shape of the first element in the array
-print ('array shape of first element inside training array: ', train_data[0].shape)
-
-#noremalize everything between 0
-min_val = tf.reduce_min(train_data)
-max_val = tf.reduce_max(train_data)
-print ('min max value ', min_val, max_val)
-#train_data = (train_data - min_val) / (max_val - min_val)
-#test_data = (test_data - min_val) / (max_val - min_val)
-print ('normalized  data ')
-print (train_data)
+print (raw_df.head())
+print ('raw shape: ', raw_df.shape)
 
 
 
 
 ## Masked Mean Squared Error 
 def mmse(y_true, y_pred):
-	#custom error function that removes zeros
+	'''custom error function that removes zeros from the array
+	'''
 	mask = y_true != 0
 	return K.mean(K.square(y_true[mask] - y_pred[mask]))
 
-#building the encoder and decoder model as a tensor flow class
 class AutoEncoder(Model):
+	'''building the encoder and decoder model as a tensorflow class
+	'''
 	def __init__(self):
 		super(AutoEncoder, self).__init__()
 
@@ -75,6 +52,7 @@ class AutoEncoder(Model):
 			layers.Dense(8, activation="relu"),
 			layers.Dense(16, activation="relu"),
 			layers.Dense(32, activation="softmax"),
+			layers.Dropout(0.3),
 			layers.Dense(16, activation="relu"),
 		])
 
@@ -94,39 +72,95 @@ class AutoEncoder(Model):
 		return decoded
 
 
-"""
-#this code works perfectly though can't tell how good the network actualy is
-autoencoder = AutoEncoder()
-autoencoder.compile(optimizer='RMSprop', loss=mmse, metrics = ['mean_squared_error'])
-#run for all epochs
-fit = autoencoder.fit(train_data, train_data,
-          epochs=70, batch_size=100, shuffle=True,
-          validation_data = (test_data, test_data),
-          verbose=1)
-"""
+def setup_data(data_frame, var_training):
+	'''this function will create the training test dataframe based on whether
+	we are going to use a fit or a train_on_batch appraoch as we need an int
+	or a float in some instances'''
 
-#https://www.tensorflow.org/api_docs/python/tf/keras/optimizers/RMSprop
+	#split train and testing data set by 80% 20%
+	(train_data, test_data) = train_test_split(raw_df, test_size=0.2)
+	print ('train data shape:', train_data.shape)
+	print ('test data shape:', test_data.shape)
+	#convert the train and test data into arrays
+	train_data = train_data.values 
+	test_data = test_data.values 
+	#look at the array shape of the first element in the array
+	print ('array shape of first element inside training array: ', train_data[0].shape)
+	#find the min and max values
+	min_val = tf.reduce_min(train_data)
+	max_val = tf.reduce_max(train_data)
 
-#train on batch attempt
-autoencoder = AutoEncoder()
-autoencoder.compile(optimizer='RMSprop', loss=mmse, metrics = ['mean_squared_error'])
-#to use the train_on_batch
-#onvert the train_data into an integer
+	if var_training == "fit":
+		print ('fit')
+		#noremalize everything between 0
+		
+		print ('min max value ', min_val, max_val)
+		train_data = (train_data - min_val) / (max_val - min_val)
+		test_data = (test_data - min_val) / (max_val - min_val)
+		print ('normalized  data ')
+		print (train_data)
+	else:
+		#we have a batch the training data needs to be a int for now and not normalized
+		#it will be normalized inside the epoch for loop 
+		# we convert and normlaize the test data however
+		print ('batch')
+		#we need to convert the training and test data into int types to be able
+		#to use the random.choice functio n
+		train_data = train_data.astype(int)
+		#test_data  = test_data/5 
+		test_data = (test_data - min_val) / (max_val - min_val)
 
-train_data = train_data.astype(int)
-test_data  = test_data/5 #.astype(int)
-for epoch in range(1, 50):
-	x = train_data[np.random.choice(train_data.shape[0], 100, replace=False)]
-	#y= test_data[np.random.choice(test_data.shape[0], 100, replace=False)]
-	#print (x.shape)
-	x_data = x/5
-	#y_data = y/5
-	loss = autoencoder.train_on_batch(x_data,x_data)
-	print ('epoch: ', epoch, 'loss: ', loss)
+	return (train_data, test_data, max_val, min_val)
 
 
-score = autoencoder.evaluate(test_data, test_data)
-print('score is', score)
+train_data, test_data, max_val, min_val = setup_data(raw_df, var_training=TRAINING)
+
+def fit_or_batch_trainer(train_data, test_data, var_training, max_max_val, min_val):
+	'''fucntion which can run the fit or train_on_batch model
+	'''
+	autoencoder = AutoEncoder()
+	autoencoder.compile(optimizer='RMSprop', loss=mmse, metrics = ['mean_squared_error'])
+	if var_training == "fit":
+		#we are running the fit model
+		print ('fit')
+		#this code works perfectly though can't tell how good the network actualy is
+		#run for all epochs
+		fit = autoencoder.fit(train_data, train_data,
+		          epochs=70, batch_size=100, shuffle=True,
+		          validation_data = (test_data, test_data),
+		          verbose=1)
+		#determine the score and accuracy of our model
+		score = autoencoder.evaluate(test_data, test_data)
+		print('score is', score)
+	else:
+		print ('batch')
+		#train on batch attempt
+		#autoencoder = AutoEncoder()
+		#autoencoder.compile(optimizer='RMSprop', loss=mmse, metrics = ['mean_squared_error'])
+		#to use the train_on_batch
+		#onvert the train_data into an integer
+
+		#train_data = train_data.astype(int)
+		#test_data  = test_data/5 
+		#loop through an epoch
+		for epoch in range(1, 250):
+			#extract random data from the training data 
+			x = train_data[np.random.choice(train_data.shape[0], 100, replace=False)]
+			#normalize the random extracted data
+			x_data = (x - min_val) / (max_val - min_val)
+			#x_data = x/5
+			#run our autoencoder for training 
+			loss = autoencoder.train_on_batch(x_data, x_data)
+			print ('epoch: ', epoch, 'loss: ', loss)
+
+		#determine the score and accuracy of our model 
+		score = autoencoder.evaluate(test_data, test_data)
+		print('score is', score)
+
+fit_or_batch_trainer(train_data, test_data, TRAINING, max_val, min_val)
+
+
+
 
 
 """
@@ -156,3 +190,4 @@ plt.savefig("plotsaved2.png")
 #https://cnvrg.io/keras-custom-loss-functions/
 #https://towardsdatascience.com/https-medium-com-chayankathuria-regression-why-mean-square-error-a8cad2a1c96f
 #https://medium.datadriveninvestor.com/generative-adversarial-network-gan-using-keras-ce1c05cfdfd3
+#https://www.tensorflow.org/api_docs/python/tf/keras/optimizers/RMSprop
